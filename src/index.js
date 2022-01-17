a59c6b60c6e07fa8c8e4c9de233a485edc42580b
mineevburyat <mineev.buryat@gmail.com>
2022-01-17 21:25:26 +0800

    lesson1 (react component, props, object on render)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const ComponentWithoutJSX = () => {
+  return React.createElement(
+    "div",
+    null,
+    React.createElement("h3", null, "NoJSX component")
+  );
+};
+
+class ClassComponent extends React.Component {
+  render() {
+    console.log(this.props);
+    const { ageold, films } = this.props;
+    return (
+      <div>
+        <h2>Hello class companent</h2>
+        {/* <p>{this.props} если раскомментировать то страница пустая, в консоле ошибка </p> */}
+        <p>
+          <span>Возраст: </span>
+          <i>{ageold}</i>
+        </p>
+        <p>Список фильмов:</p>
+        <ul>
+          {films.map((film) => {
+            return <li>{film}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+function FunctionComponent({ ageold, films }) {
+  return (
+    <div>
+      <h2>Hello function component</h2>
+      <p>
+        <span>Возраст: </span>
+        <i>{ageold}</i>
+      </p>
+      <p>Список фильмов:</p>
+      <ul>
+        <li>{films[0]}</li>
+        <li>{films[1]}</li>
+      </ul>
+    </div>
+  );
+}
+
+const Parent = () => {
+  const age = 100;
+  const listFilms = ["film1", "film2"];
+  return (
+    <div>
+      <h1>Parent block</h1>
+      <ClassComponent ageold={age} films={listFilms} />
+      <FunctionComponent ageold={age} films={listFilms} />
+      <ComponentWithoutJSX />
+    </div>
+  );
+};
+ReactDOM.render(
+  <React.StrictMode>
+    <Parent />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Second app</h1>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("secondroot")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
