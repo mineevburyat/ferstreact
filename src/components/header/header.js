22efaa473fa4a6287d8d2709b2fe4ffc33575245
mineevburyat <mineev.buryat@gmail.com>
2022-01-26 20:55:45 +0800

    new react

--- src/components/header/header.js
+++
@@ -1,9 +0,0 @@
-import styles from "./header.module.css";
-
-export function Header() {
-  return (
-    <div className={styles.header}>
-      <h1>header </h1>
-    </div>
-  );
-}
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./message-list";
-export * from "./chat-list";
-export * from "./header";
-export * from "./layout";
