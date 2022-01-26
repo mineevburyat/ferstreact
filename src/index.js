2362d3be46be86bac55872791b9928b06b9023d7
mineevburyat <mineev.buryat@gmail.com>
2022-01-26 22:54:17 +0800

    layout applications

--- src/index.js
+++ src/index.js
@@ -1,9 +1,99 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// matrial ui
+// grid layout
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+// app bar
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+//left aside with rooms
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+// Input field and send box
+import TextField from '@mui/material/TextField';
+
+
+export function ButtonAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Chat
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export function BasicList() {
+  return (
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <nav aria-label="main mailbox folders">
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Room 1" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Room 2" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemText primary="Room 3" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
+      <Divider />
+    </Box>
+  );
+}
 
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid item xs={12}>
+          <ButtonAppBar/>
+        </Grid>
+        <Grid item xs={4}>
+          <BasicList/>
+        </Grid>
+        <Grid item xs={8} sx={{ height:'85hv'}}>
+          <Box  sx={{ height: '85hv', width: '100%', bgcolor: 'background.paper' }}></Box>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField  sx={{ width: '80%', bgcolor: 'background.paper' }} id="inputtext" label="enter text..." variant="standard" />
+          <Button variant="contained">send</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 
